6e5e33073cdad52904b155c91c9c603da51b6dae
devdave0 <dbchung24@gmail.com>
2020-08-14 11:36:37 -0400

    added a signup page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import CategoryContainer from './containers/CategoryContainer'
 import TaskContainer from './containers/TaskContainer'
 import TaskListContainer from './containers/TaskListContainer'
+import SignUp from './SignUp'
 
 class App extends React.Component {
   render(){
     return (
       <div className="App">
         <header className="App-header">
+          <SignUp />
           <h1>TO DO LIST</h1>
           <TaskListContainer />
           <CategoryContainer />
