9ed983957c3b3c78d4e46581015673830519fab2
devdave0 <dbchung24@gmail.com>
2020-08-17 16:13:15 -0400

    started adding semantic ui to login page

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
 
 class Login extends Component {
@@ -35,17 +36,45 @@ class Login extends Component {
 
     render() {
         return(
-            <div>
-                <h2>Login</h2>
-                <form onSubmit={(e)=> this.handleSubmit(e)}>
-                    <label>Username: </label>
-                    <input onChange={(e) => this.handleChange(e)} name='name' type='text'/>
-                    <label>Password: </label>
-                    <input onChange={(e) => this.handleChange(e)} name='password' type='password'/>
-                    <input type='submit'/>
-                </form>
-                < Link to='/sign_up'>Sign Up</Link>
-            </div>
+            // <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+            //     <h2>Login</h2>
+            //     <form onSubmit={(e)=> this.handleSubmit(e)}>
+            //         <label>Username: </label>
+            //         <input onChange={(e) => this.handleChange(e)} name='name' type='text'/>
+            //         <label>Password: </label>
+            //         <input onChange={(e) => this.handleChange(e)} name='password' type='password'/>
+            //         <input type='submit'/>
+            //     </form>
+            //     < Link to='/sign_up'>Sign Up</Link>
+            // </Grid>
+
+            <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+    <Grid.Column style={{ maxWidth: 450 }}>
+      <Header as='h2' color='teal' textAlign='center'>
+        <Image src='/logo.png' /> Log-in to your account
+      </Header>
+      <Form size='large'>
+        <Segment stacked>
+          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+          <Form.Input
+            fluid
+            icon='lock'
+            iconPosition='left'
+            placeholder='Password'
+            type='password'
+          />
+
+          <Button color='teal' fluid size='large'>
+            Login
+          </Button>
+        </Segment>
+      </Form>
+      <Message>
+        New to us? <a href='#'>Sign Up</a>
+      </Message>
+    </Grid.Column>
+  </Grid>
+
         )
     }
 
